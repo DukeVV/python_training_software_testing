9e8e6da886b4fa32a60aa55a962c5bf9d8ab4971
DukeVV <46594370+DukeVV@users.noreply.github.com>
2019-02-05 09:33:56 +0400

    файлы фикстуры

--- contact.py
+++ contact.py
@@ -1,23 +1,76 @@
-from sys import maxsize
+
+from model.contact import Contact
 
 
-class Contact:
+class ContactHelper:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, id=None):
-        self.firstname = firstname
-        self.middlename = middlename
-        self.lastname = lastname
-        self.id = id
+    def __init__(self, app):
+        self.app = app
 
-    def __repr__(self):
-        return "%s:%s" % (self.id, self.lastname)
+    def create_new(self, contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_link_text("add new").click()
+        self.fill_name(contact)
+        # input data base
+        wd.find_element_by_xpath("//input[21]").click()
+        self.contact_cache = None
 
-    def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname
+    def fill_name(self, contact):
+        wd = self.app.wd
+        self.change_contact_field_value("firstname", contact.firstname)
+        self.change_contact_field_value("middlename", contact.middlename)
+        self.change_contact_field_value("lastname", contact.lastname)
 
-    def id_or_max(self):
-        if self.id:
-            return int(self.id)
-        else:
-            return maxsize
+    def change_contact_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit delete
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_xpath("//div[@id='content']/div").click()
+        self.contact_cache = None
+
+    def modify_first_contact(self, new_contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.fill_name(new_contact)
+        # input data base
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("home page").click()
+        self.contact_cache = None
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+    def check_contact(self):
+        wd = self.app.wd
+        if self.count() == 0:
+            self.create_new(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov"))
+
+    contact_cache = None
+
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                cells = element.find_elements_by_tag_name("td")
+                text = cells[1].text
+                id = element.find_element_by_name("selected[]"). get_attribute("value")
+                self.contact_cache.append(Contact(lastname=text, id=id))
+        return list(self.contact_cache)
 
